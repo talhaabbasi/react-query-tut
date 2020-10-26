57d6cd60846abb8da92e250078586295ddad2417
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-10-26 13:47:05 +0500

    Add react-query and multiple components

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Person from "./Person";
+
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
 
 export default function People() {
+  const { data, status } = useQuery("people", fetchPeople);
+  console.log(status);
   return (
     <div>
       <h2>People</h2>
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((person) => {
+            return <Person person={person} key={person.name} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets/");
+  return res.json();
+};
 
 export default function Planets() {
+  const { data, status } = useQuery("planets", fetchPlanets);
+  console.log(status);
   return (
     <div>
       <h2>Planets</h2>
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((planet) => {
+            return <Planet planet={planet} key={planet.name} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
